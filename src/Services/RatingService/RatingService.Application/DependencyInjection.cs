dcab2d1772d2e7fd6de6ab83e8411dee1be27898
mertunaler <mertunaler@gmail.com>
2024-12-16 00:22:01 +0300

    feat: depInjector added for appLayer

---
+++ src/Services/RatingService/RatingService.Application/DependencyInjection.cs
@@ -0,0 +1,16 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RatingService.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(options =>
+        {
+            options.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+        });
+
+        return services;
+    }
+}
