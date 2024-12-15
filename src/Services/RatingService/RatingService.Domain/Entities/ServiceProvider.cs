7d9a46e83a937ca72a8e26c1187ae99cbad21d6a
mertunaler <mertunaler@gmail.com>
2024-12-16 00:07:06 +0300

    fix: columns are publicly settable

--- src/Services/RatingService/RatingService.Domain/Entities/ServiceProvider.cs
+++ src/Services/RatingService/RatingService.Domain/Entities/ServiceProvider.cs
@@ -2,8 +2,7 @@ namespace RatingService.Domain.Entities;
 
 public class ServiceProvider
 {
-    public int Id { get; private set; }
-    public string Name { get; private set; }
-    public ICollection<Rating> Ratings { get; private set; }
-    
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public ICollection<Rating> Ratings { get; set; }
 }
